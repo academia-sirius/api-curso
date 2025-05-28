8feb842cbffeccddae60e749341322ed1cd858e7
Adilson Futa <adilsonmariafuta@hotmail.com>
2025-05-28 17:33:08 +0200

    learning controller service module and bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const portNumber = 3000;
-async function bootstrap() {
-const app = await NestFactory.create(AppModule);
+async function main() {
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? portNumber);
-}
-bootstrap();
+} 
+main();
